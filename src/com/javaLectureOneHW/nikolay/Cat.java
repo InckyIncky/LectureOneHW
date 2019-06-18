3eeb7872fa86d55c6302c78bd6593228de6798f1
nantonov <inckyincky049@gmail.com>
2019-06-18 17:02:33 +0300

    Initial commit

---
+++ src/com/javaLectureOneHW/nikolay/Cat.java
@@ -0,0 +1,61 @@
+package com.javaLectureOneHW.nikolay;
+
+import com.javaLectureOneHW.nikolay.Animal;
+
+public class Cat extends Animal {
+    String breed;
+    int age;
+    double weight;
+    String name;
+    String says;
+    boolean wild;
+
+    public Cat(String breed, int age, double weight, String name, String says, boolean wild){
+        breed = breed;
+        age = age;
+        weight = weight;
+        name = name;
+        says = says;
+        wild = wild;
+
+    }
+
+    public void setWild (boolean wild) {
+        this.wild = wild;
+    }
+    public boolean getWild (boolean wild) {
+        return this.wild;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    public String getBreed() {
+        return this.breed;
+    }
+    public void setAge(int Age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public double getWeight() {
+        return this.weight;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setSays(String says) {
+        this.says = says;
+    }
+    public String getSays() {
+        return this.says;
+    }
+
+
+}
